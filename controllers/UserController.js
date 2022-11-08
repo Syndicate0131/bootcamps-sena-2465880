fd4bf9921365d41657375b9e659a912ce947545e
Juan Quitian <you@example.com>
2022-11-08 09:53:32 -0500

    Validacion Errores

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,7 +2,7 @@
 const sequelize = require('../config/seq')
 
 // DataTypes 
-const { DataTypes } = require ('sequelize')
+const { DataTypes , ValidationError } = require ('sequelize')
 
 // Modelo A Trabajar 
 const UserModel = require ('../models/user')
@@ -13,53 +13,142 @@ const User = UserModel(sequelize, DataTypes)
 
 // Get: Obtener datos -  Read
 exports.traerUsers = async(req, res)=>{
-    const users = await User.findAll();
-    res.status(200).json(
-        {
-            "success" : true,
-            "data" : users
-        }
-    )
+    try {
+        const users = await User.findAll();
+        res.status(200).json(
+            {
+                "success" : true,
+                "data" : users
+            }
+        )        
+    } catch (error) {
+        //Errores de Servidor
+        res
+        .status(500)
+        .json({
+            "success":false,
+            "errors": "Error del Servidor"
+        })  
+    }
+
 }
 
 // Obtener recurso por id
 exports.traerUsersPorId = async(req, res)=>{
-    const userId = await User.findByPk(req.params.id)
-    res.status(200).json(
-        {
-            "success" : true,
-            "data" : userId
+    try {
+        const userId = await User.findByPk(req.params.id)
+        //Si el usuario no existe
+        if (!userId) {
+            res.status(422).json(
+                {
+                    "success" : false,
+                    "data" : [
+                        "Usuario no Existe"
+                    ]
+                }
+            )     
+        }else{
+        res.status(200).json(
+            {
+                "success" : true,
+                "data" : userId
+            }
+        )   
         }
-    )
+    } catch (error) {
+        //Errores de Servidor
+        res
+        .status(500)
+        .json({
+            "success":false,
+            "errors": "Error del Servidor"
+        })  
+    }
 }
 
 // Post: Crear un nuevo recurso - Create 
 exports.crearUser = async(req, res)=>{
-    const newUser = await User.create(req.body);
-    res.status(201).json({
-        "success" : true,
-        "data" : newUser
-    })
+    try {
+        const newUser = await User.create(req.body);
+        res.status(201).json({
+            "success" : true,
+            "data" : newUser
+        })
+    } catch (error) {
+        if(error instanceof ValidationError){
+            //Mensajes de Errores
+            const errores = error.errors.map((e)=> e.message )
+            // Llevar errores al response 
+            res
+            .status(422)
+            .json({
+                "success":false,
+                "errors": errores
+            })
+        }else{
+            //Errores de Servidor
+            res
+            .status(500)
+            .json({
+                "success":false,
+                "errors": "Error del Servidor"
+            })
+        }
+    }   
 }
 
 //Put - Patch : Actualiza un recurso - Update
 exports.actualizarUser = async(req, res)=>{
-    // Actualizar Usuario Por Id
-    await User.update(req.body,{
-    where:{
-        id: req.params.id
+    try {
+        // Consultar Datos Actualizados
+        const upUser = await User.findByPk(req.params.id)
+        if (!upUser) {
+            res.status(422).json(
+                {
+                    "success" : false,
+                    "data" : [
+                        "Usuario no Existe"
+                    ]
+                }
+            )     
+        }else{
+            // Actualizar Usuario Por Id
+            await User.update(req.body,{
+            where:{
+                id: req.params.id
+                }
+            });
+            const upAct = await User.findByPk(req.params.id)
+        res.status(200).json(
+            {
+                "success" : true,
+                "data" : upAct
+    
+            }
+        )
         }
-    });
-    // Consultar Datos Actualizados
-    const upUser = await User.findByPk(req.params.id)
-
-    res.status(200).json(
-        {
-            "success" : true,
-            "data" : upUser
-
+    } catch (error) {
+        if(error instanceof ValidationError){
+            //Mensajes de Errores
+            const errores = error.errors.map((e)=> e.message )
+            // Llevar errores al response 
+            res
+            .status(422)
+            .json({
+                "success":false,
+                "errors": errores
+            })
+        }else{
+            //Errores de Servidor
+            res
+            .status(500)
+            .json({
+                "success":false,
+                "errors": "Error del Servidor"
+            })
         }
-    )
+    }
+
 }
 
 //Delete : Elimina un recurso - Delete
--- models/user.js
+++ models/user.js
@@ -14,10 +14,66 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
+    name: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isAlpha: {
+          args:true,
+          msg: 'El campo Name solo acepta caracteres alfabeticos'
+        },
+        notEmpty: {
+          args:true,
+          msg: 'El campo Name no puede estar vacio'
+        },   
+        notNull: {
+          args:true,
+          msg: 'El registro debe llevar el campo Name'
+        },   
+
+      }
+    },
+    email:{
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          args:true,
+          msg: 'El campo Email tiene que ser digitado con formato Email'
+        },
+        notEmpty: {
+          args:true,
+          msg: 'El campo Email no puede estar vacio'
+        },   
+        notNull: {
+          args:true,
+          msg: 'El registro debe llevar el campo Email'
+        },   
+      }
+    },
+    password:{
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len:{
+          args: [5,15],
+          msg:'El campo debe tener entre 5 a 15 caracteres de longitud'
+        },
+        notEmpty: {
+          args:true,
+          msg: 'El campo password no puede estar vacio'
+        },   
+        notNull: {
+          args:true,
+          msg: 'El registro debe llevar el campo password'
+        },   
+      }
+    },
+  }, 
+  {
     sequelize,
     modelName: 'User',
     timestamps: false
